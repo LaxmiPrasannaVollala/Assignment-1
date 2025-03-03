9e511d09826e896b93b5007f7ede688bded0e510
Laxmi Prasanna Vollala <laxmiprasanna13.v@gmail.com>
2025-03-03 17:27:15 -0600

    Update data_load.py

--- Question1/Frailty_Project/src/data_load.py
+++ Question1/Frailty_Project/src/data_load.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import ttest_ind, pearsonr
+
+df = pd.read_csv("/content/raw_data_frailty.csv")
+df.shape
+df.info()
+df.dtypes
+
 
