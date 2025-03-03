25be5f34ff4c2fcf0170c78d669b639689d32a43
Laxmi Prasanna Vollala <laxmiprasanna13.v@gmail.com>
2025-03-03 17:29:11 -0600

    Update data_clean.py

--- Question1/Frailty_Project/src/data_clean.py
+++ Question1/Frailty_Project/src/data_clean.py
@@ -1,1 +1,6 @@
-
+#checking for null values
+df.isnull().sum()
+# Convert 'Frailty' to binary (0 = No, 1 = Yes)
+df['Frailty'] = df['Frailty'].map({'N': 0, 'Y': 1})
+df
+df.to_csv('cleaned_.csv', index=False)
